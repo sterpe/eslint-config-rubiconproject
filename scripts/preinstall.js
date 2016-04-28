5ab5a8c404612c550fea8f9560fcb025e6fd9a18
Steve Terpe <sterpe@rubiconproject.com>
2016-04-28 22:35:33 +0800

    Try to run in the correct dir

--- scripts/preinstall.js
+++ scripts/preinstall.js
@@ -2,6 +2,8 @@
 
 var peerDependencies = require('../package').peerDependencies;
 var child_process = require('child_process'); // eslint-disable-line camelcase
+var path = require('path');
+var cwd = process.cwd();
 var L = [];
 
 for (var key in peerDependencies) {
@@ -10,10 +12,16 @@ for (var key in peerDependencies) {
     }
 }
 
+while (cwd.indexOf('node_modules') !== -1) {
+    cwd = path.dirname(cwd);
+}
 if (L.length > 0) {
     L = ['install', '--save-dev'].concat(L);
     var error = child_process // eslint-disable-line camelcase
-        .spawnSync('npm', L, { stdio: 'inherit' }).error;
+        .spawnSync('npm', L, {
+            stdio: 'inherit',
+            cwd: cwd
+        }).error;
     if (error) {
         process.exit(1);
     }
